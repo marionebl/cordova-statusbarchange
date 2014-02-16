ff38e7c8499e5d5f54062bbee15623ac8c565f9f
Mario Nebl <mario@bloozone.com>
2014-02-16 23:34:43 +0100

    Initial commit.

---
+++ platforms/ios/SocialConductor/Classes/MainViewController.m
@@ -0,0 +1,222 @@
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
+
+//
+//  MainViewController.h
+//  SocialConductor
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString*)nibNameOrNil bundle:(NSBundle*)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Uncomment to override the CDVCommandDelegateImpl used
+        // _commandDelegate = [[MainCommandDelegate alloc] initWithViewController:self];
+        // Uncomment to override the CDVCommandQueue used
+        // _commandQueue = [[MainCommandQueue alloc] initWithViewController:self];
+    }
+    return self;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Uncomment to override the CDVCommandDelegateImpl used
+        // _commandDelegate = [[MainCommandDelegate alloc] initWithViewController:self];
+        // Uncomment to override the CDVCommandQueue used
+        // _commandQueue = [[MainCommandQueue alloc] initWithViewController:self];
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark View lifecycle
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    // View defaults to full size.  If you want to customize the view's size, or its subviews (e.g. webView),
+    // you can do so here.
+
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                          selector:@selector(onStatusbarChange:)
+                                          name: @"statusbarchange"
+                                          object:nil];
+}
+
+    - (void)onStatusbarChange:(NSNotification*)notification
+    {
+        // Native code for
+        NSMutableDictionary *eventInfo = [self getStatusBarInfo];
+        [self notifiy:notification.name withInfo:eventInfo];
+    }
+
+    - (void)notifiy:(NSString*)event withInfo:(NSMutableDictionary*)info
+    {
+        NSString *json = [self toJSON:info];
+        NSString *cmd = [NSString stringWithFormat:@"cordova.fireWindowEvent('\%@\', %@)", event, json];
+        [self.webView stringByEvaluatingJavaScriptFromString:cmd];
+    }
+
+    - (NSMutableDictionary *)getStatusBarInfo
+    {
+        CGRect statusBarFrame = [[UIApplication sharedApplication] statusBarFrame];
+
+        NSMutableDictionary *statusBarInfo = [[NSMutableDictionary alloc] init];
+        NSMutableDictionary *size = [[NSMutableDictionary alloc] init];
+        NSMutableDictionary *origin = [[NSMutableDictionary alloc] init];
+
+        size[@"height"] = [NSNumber numberWithInteger:((int) statusBarFrame.size.height)];
+        size[@"width"] = [NSNumber numberWithInteger:((int) statusBarFrame.size.width)];
+
+        origin[@"x"] = [NSNumber numberWithInteger:((int) statusBarFrame.origin.x)];
+        origin[@"y"] = [NSNumber numberWithInteger:((int) statusBarFrame.origin.y)];
+
+        statusBarInfo[@"size"] = size;
+        statusBarInfo[@"origin"] = origin;
+
+        return statusBarInfo;
+    }
+
+    - (NSString *) toJSON:(NSDictionary *)dictionary {
+        NSError *error;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
+        return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    }
+
+    - (void)dealloc
+    {
+        [[NSNotificationCenter defaultCenter] removeObserver:self];
+    }
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return [super shouldAutorotateToInterfaceOrientation:interfaceOrientation];
+}
+
+/* Comment out the block below to over-ride */
+
+/*
+- (UIWebView*) newCordovaViewWithFrame:(CGRect)bounds
+{
+    return[super newCordovaViewWithFrame:bounds];
+}
+*/
+
+#pragma mark UIWebDelegate implementation
+
+- (void)webViewDidFinishLoad:(UIWebView*)theWebView
+{
+    // Black base color for background matches the native apps
+    theWebView.backgroundColor = [UIColor blackColor];
+
+    return [super webViewDidFinishLoad:theWebView];
+}
+
+/* Comment out the block below to over-ride */
+
+/*
+
+- (void) webViewDidStartLoad:(UIWebView*)theWebView
+{
+    return [super webViewDidStartLoad:theWebView];
+}
+
+- (void) webView:(UIWebView*)theWebView didFailLoadWithError:(NSError*)error
+{
+    return [super webView:theWebView didFailLoadWithError:error];
+}
+
+- (BOOL) webView:(UIWebView*)theWebView shouldStartLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    return [super webView:theWebView shouldStartLoadWithRequest:request navigationType:navigationType];
+}
+*/
+
+@end
+
+@implementation MainCommandDelegate
+
+/* To override the methods, uncomment the line in the init function(s)
+   in MainViewController.m
+ */
+
+#pragma mark CDVCommandDelegate implementation
+
+- (id)getCommandInstance:(NSString*)className
+{
+    return [super getCommandInstance:className];
+}
+
+/*
+   NOTE: this will only inspect execute calls coming explicitly from native plugins,
+   not the commandQueue (from JavaScript). To see execute calls from JavaScript, see
+   MainCommandQueue below
+*/
+- (BOOL)execute:(CDVInvokedUrlCommand*)command
+{
+    return [super execute:command];
+}
+
+- (NSString*)pathForResource:(NSString*)resourcepath;
+{
+    return [super pathForResource:resourcepath];
+}
+
+@end
+
+@implementation MainCommandQueue
+
+/* To override, uncomment the line in the init function(s)
+   in MainViewController.m
+ */
+- (BOOL)execute:(CDVInvokedUrlCommand*)command
+{
+    return [super execute:command];
+}
+
+@end
